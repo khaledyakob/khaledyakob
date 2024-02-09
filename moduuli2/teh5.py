6ec4ba1cd425a1d187984f42ed93ea3d1dc0de38
Khale <khaled.hussein@metropolia.fi>
2024-02-09 14:33:39 +0200

    ui

---
+++ moduuli2/teh5.py
@@ -0,0 +1,9 @@
+import math
+
+print("keskiaikaisten mitat")
+print("yksi leiviksä on 20 naulaa")
+print("yksi naula on 32 luotia")
+print("yksi luoti on 13,3 grammaa")
+
+
+input("anna massan keskiaikaisten mitat: ")
