370ee1bfe4566624df70d098ec0fda86c0ae3bb9
Khale <khaled.hussein@metropolia.fi>
2024-02-09 14:32:12 +0200

    ui

---
+++ moduuli2/teh4.py
@@ -0,0 +1,14 @@
+eka = int(input("anna kokonaisluku"))
+print(f"kokonaisluku {eka}")
+
+toka = int(input("anna kokonaisluku"))
+print(f"kokonaisluku {toka}")
+
+kolmas= int(input("anna kokonaisluku"))
+print(f"kokonaisluku {kolmas}")
+
+summa = eka + toka + kolmas
+
+print(f"lukujen {summa}")
+print(f"lukujen tulos {eka * toka * kolmas}")
+print(f"lukujen keskiarvo {(eka +toka + kolmas)/3}")
