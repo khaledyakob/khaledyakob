7c479ec6fa2714b4ce74a312668803b3c39e469a
Khale <khaled.hussein@metropolia.fi>
2024-02-09 14:34:54 +0200

    ui

---
+++ moduuli2/teh6.py
@@ -0,0 +1,10 @@
+import random
+eka = random.randint(0,9)
+toka = random.randint(0,9)
+kolmas = random.randint(0,9)
+
+
+eka = random.randint(1,6)
+toka = random.randint(1,6)
+kolmas = random.randint(1,6)
+neljäs = random.randint(1,6)
